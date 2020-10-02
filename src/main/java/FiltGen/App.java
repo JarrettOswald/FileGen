9f68254f722db8975abec7f701762c446609521f
JarrettOswald <54551519+JarrettOswald@users.noreply.github.com>
2020-10-02 11:54:13 +0400

    add

--- src/main/java/FiltGen/App.java
+++ src/main/java/FiltGen/App.java
@@ -21,15 +21,14 @@ public class App
 
 
     public static void main( String[] args ) throws IOException, DocumentException {
-        GenImg.genImg("жопа","10.000001","jpeg");
-        GenPDF.genPDF("Для тебя Аня 80.pdf","test LANIT","80");
-        GenDOC.genDoc("Заявление на вычет01","Говно","1");
-        GenTxt.genTxt("Текстовый01","LANIT TEST TXT","1");
-            long m = System.currentTimeMillis();
-        GenImg.genImg("5jpg","5","jpg");
+        GenImg.genImg("-299dpi","1","jpeg",1765,2354);
+//        GenPDF.genPDF("Для тебя Аня 80.pdf","test LANIT","80");
+//        GenDOC.genDoc("Заявление на вычет01","Говно","1");
+//        GenTxt.genTxt("Текстовый01","LANIT TEST TXT","1");
+//            long m = System.currentTimeMillis();
 
 
 //        FileGenerator.convertByte("8.000001");
-        System.out.println((double) (System.currentTimeMillis() - m));
+//        System.out.println((double) (System.currentTimeMillis() - m));
     }
 }
--- src/main/java/FiltGen/GenImg.java
+++ src/main/java/FiltGen/GenImg.java
@@ -7,18 +7,17 @@ import java.io.*;
 
 public class GenImg extends FileGenerator {
 
-    public static void genImg(String nameFile, String size, String format) throws IOException {
+    public static void genImg(String nameFile, String size, String format, int width, int height) throws IOException {
 
-//        BufferedImage bufferedImage = new BufferedImage(320, 400, BufferedImage.TYPE_INT_RGB);
-////        BufferedImage bufferedImage = new BufferedImage(new FileReader(""));
-//        Graphics2D g2d = bufferedImage.createGraphics();
-//        g2d.drawString(nameFile, 0, 20);
-//        g2d.drawString(size + "мб", 0, 60);
-//        g2d.dispose();
-//        File file = new File(PATHCREATE + nameFile + "." + format);
-//        ImageIO.write(bufferedImage, "tiff", file);
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.drawString(nameFile, 0, 20);
+        g2d.drawString(size + "мб", 0, 60);
+        g2d.dispose();
+        File file = new File(PATHCREATE + nameFile + "." + format);
+        ImageIO.write(bufferedImage, format, file);
 
-        RandomAccessFile randomAccessFile = new RandomAccessFile("C:\\Users\\Korepanov\\Desktop\\5.jpeg","rw");
+        RandomAccessFile randomAccessFile = new RandomAccessFile(PATHCREATE + nameFile + "." + format, "rw");
         randomAccessFile.setLength(FileGenerator.convertByte(size));
         randomAccessFile.close();
     }
